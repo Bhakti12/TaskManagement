3ae6ec9661ee73fb26f25e880ddf7082eb96f02c
Bhakti12 <sanghanibhakti922@gmail.com>
2024-06-06 03:06:37 +0530

    implementing interfaces

--- src/interface/ITask.repository.ts
+++ src/interface/ITask.repository.ts
@@ -1,3 +1,10 @@
+import { Task, User } from "../types/task"
+
 export interface ITaskRepository {
-    
+    createUser(user: User): Promise<User>
+    createTask(task: Task, user_id: string): Promise<Task>
+    getTasksOfUser(user_id: string): Promise<Task[]>
+    getTaskOfUser(user_id: string, task_id: string): Promise<Task>
+    updateTaskOfUser(user_id: string, task_id: string): Promise<Task>
+    deleteTaskOfUser(user_id: string, task_id: string): Promise<Task>
 }
