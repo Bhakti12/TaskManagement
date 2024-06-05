bfc98d48ca05a897b98acc94f94e3607383dd7ef
Bhakti12 <sanghanibhakti922@gmail.com>
2024-06-06 02:48:21 +0530

    project setup is done

---
+++ src/types/task.ts
@@ -0,0 +1,17 @@
+export type TaskStatus = 'To-DO' | 'In-Progress' | 'DONE';
+
+export declare type Task = {
+    id: string;
+    title: string;
+    description: string;
+    date: Date;
+    status: TaskStatus;
+};
+
+export declare type User = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    tasks: Task[];
+};
